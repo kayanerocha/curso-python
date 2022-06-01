3398dd3f92d6bcf5972a5cf2ea282aace0fe19d5
kayanerocha <kayannerch@gmail.com>
2022-06-01 12:50:50 -0300

    adicionando a solução do guanabara

--- mundo3/exercicios/ex080.py
+++ mundo3/exercicios/ex080.py
@@ -28,3 +28,24 @@ for i in range(0, 5):
 print('-=' * 30)
 print(f'Os valores digitados em ordem foram {valores}')
 
+
+
+# Solução do Guanabara
+
+lista = []
+for c in range(0, 5):
+    numero = int(input('Digite um valor: '))
+    if c == 0 or numero > lista[-1]:
+        lista.append(numero)
+        print('Adicionado ao final da lista...')
+    else:
+        posicao = 0
+        while posicao < len(lista):
+            if numero <= lista[posicao]:
+                lista.insert(posicao, numero)
+                print(f'Adicionado na posição {posicao} da lista...')
+                break
+            posicao += 1
+
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {lista}')
