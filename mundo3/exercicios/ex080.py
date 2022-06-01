117fe66b80e0da305427edc9a051d26143ac9037
kayanerocha <kayannerch@gmail.com>
2022-06-01 12:02:58 -0300

    corrigindo a ordenação dos números na lista

--- mundo3/exercicios/ex080.py
+++ mundo3/exercicios/ex080.py
@@ -1,17 +1,30 @@
 menor = 0
+posicao_menor = 0
 valores = []
+is_menor = False
+
 for i in range(0, 5):
-    valor = int(input('Valor: '))
+    valor = int(input('Digite um Valor: '))
+
     if len(valores) == 0:
         valores.append(valor)
-        menor = valor
+        print('Adicionado ao final da lista...')
     else:
         for p, v in enumerate(valores):
             if valor < v:
+                is_menor = True
                 menor = valor
-                valores.insert(p, valor)
+                posicao_menor = p
                 break
             else:
-                valores.append(valor)
-                break
-print(valores)
+                is_menor = False
+        if is_menor:
+            valores.insert(posicao_menor, menor)
+            print(f'Adicionado na posição {posicao_menor} da lista...')
+        else:
+            valores.append(valor)
+            print('Adicionado ao final da lista...')
+
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {valores}')
+
