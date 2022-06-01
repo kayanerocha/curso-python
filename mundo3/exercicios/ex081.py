f6043a95b28e2e0d39608b1d8fd70b19f0c055df
kayanerocha <kayannerch@gmail.com>
2022-06-01 12:10:19 -0300

    imprimindo a posição do 5 e informando caso não digitado ele

--- mundo3/exercicios/ex081.py
+++ mundo3/exercicios/ex081.py
@@ -10,7 +10,11 @@ while True:
         break
 
 print(f'Você digitou {len(numeros)} elementos.')
+
 numeros.sort(reverse=True)
 print(f'Os valores em ordem decrescente são {numeros}')
+
 if 5 in numeros:
-    print('O valor 5 faz parte da lista!')
+    print(f'O valor 5 faz parte da lista na posição {numeros.index(5)}!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
