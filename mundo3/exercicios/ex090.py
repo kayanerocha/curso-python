686e6adce9fa37d1845da40990972e6a50c06fb8
kayanerocha <kayannerch@gmail.com>
2022-06-07 12:13:41 -0300

    guardando informações de um aluno em um dicionário

---
+++ mundo3/exercicios/ex090.py
@@ -0,0 +1,11 @@
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print(f'Nome é igual a {aluno["nome"]}')
+print(f'Média é igual a {aluno["media"]}')
+print(f'Situação é igual a {aluno["situacao"]}')
