2f65c9c5fabca75bb5cb1c2dd0cabc976830c0a9
kayanerocha <kayannerch@gmail.com>
2022-06-15 15:12:48 -0300

    adicionando docstrings nas funções

--- mundo3/exercicios/ex106.py
+++ mundo3/exercicios/ex106.py
@@ -6,6 +6,12 @@ from time import sleep
 
 
 def mostraTitulo(titulo, cor):
+    """
+    -> Mostra os títulos do programa.
+    :param titulo: título que deseja imprimir.
+    :param cor: cor de fundo para o título.
+    :return: sem retorno.
+    """
     print(f'\033[{cor}m')
     print(f'~' * (len(titulo) + 4))
     print(f'  {titulo}  ')
@@ -14,6 +20,11 @@ def mostraTitulo(titulo, cor):
     sleep(0.5)
 
 def interactiveHelp(comando):
+    """
+    -> Mostra o manual de um comando.
+    :param comando: comando que o usuário deseja ver o manual.
+    :return: sem retorno.
+    """
     mostraTitulo(f"Acessando o manual do comando '{comando}'", 46)
     
     print('\033[0;30;47m')
