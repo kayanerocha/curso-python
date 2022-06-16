41c144ffe90e4eae3e7fe81a6198480d1320218e
kayanerocha <kayannerch@gmail.com>
2022-06-16 12:52:48 -0300

    criando um modulo e importando as funcoes

---
+++ mundo3/exercicios/ex107/moeda.py
@@ -0,0 +1,13 @@
+def aumentar(valor, porcentagem):
+    novo_valor = valor + ((valor * porcentagem) / 100)
+    return novo_valor
+
+def diminuir(valor, porcentagem):
+    novo_valor = valor - ((valor * porcentagem) / 100)
+    return novo_valor
+
+def dobro(valor):
+    return valor * 2
+
+def metade(valor):
+    return valor / 2
